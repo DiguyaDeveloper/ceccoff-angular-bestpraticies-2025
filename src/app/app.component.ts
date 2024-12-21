112b725d5a4933456e767cc914436d769d4893cc
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-12-20 23:45:55 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-angular-sonar-best-practicies';
+  title = 'my-angular-sonar-best-practicies'
 }
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://staging.api.myapp.com',
+  featureFlag: true,
+}
